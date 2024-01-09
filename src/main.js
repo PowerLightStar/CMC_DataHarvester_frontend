c50e472f2daa286bff7088bbae036b0bd105ef01
CheungLight <sc.kaap.san@gmail.com>
2024-01-08 18:41:11 -0700

    update to set up

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import { createApp } from 'vue'
-import { createWebHistory } from 'vue-router'
-
-import createRouter from './pages/routes.js'
+import './style.css'
 import App from './App.vue'
 
-import './style.css'
+createApp(App).mount('#app')
 
-const router = createRouter(createWebHistory())
-const app = createApp(App)
-app.use(router).mount('#app')
